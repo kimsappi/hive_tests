75b6b2a49795eb013f192a768210405b6a5d723b
kimsappi <sappikim@gmail.com>
2020-02-02 21:04:20 -0800

    Added tests for custom printf functionality

--- printf.c
+++ printf.c
@@ -142,5 +142,69 @@ ret1 = ft_printf("#%-15p#\n", &d);
 ret2 = printf("#%-15p#\n", &d);
 ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
 
+/*
+printf("\n\n\n\n#############\nBONUSES\n#############\n\n\n");
+ft_printf("Binary:\n");
+int j;
+for (unsigned int i = 0; i < 17; ++i) {
+	ft_printf("%d: #", i);
+	j = ft_printf("%b", i);
+	ft_printf("# (return value between #s = %d)\n", j);
+}
+ft_printf("%d: %b\n", 255, 255);
+ft_printf("%d: %b\n", 256, 256);
+ft_printf("%d: %b\n", 257, 257);
+int x = 9;
+ft_printf("%d 0-padded to width 8: #", x);
+int y = ft_printf("%08b", x);
+ft_printf("# (return value between #s = %d)\n", y);
+
+ft_printf("\n\n####\nTESTING COLORS\n#####\n\n");
+ret1 = ft_printf("%{blue}blue%{}normal\n");
+ret2 = printf("\e[44mblue\e[0mnormal\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("%{blue}bl%[bred]ue%{}normal\n");
+ret2 = printf("\e[44mbl\e[1;91mue\e[0mnormal\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("%{yellow}bl%[red]ue%[]normal\n");
+ret2 = printf("\e[43mbl\e[0;91mue\e[0mnormal\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("redbg%[bblue]%{bred}blue%{}normal\n");
+ret2 = printf("redbg\e[1;94m\e[41mblue\e[0mnormal\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("%{yellowbl%[red]ue%[]normal\n");
+ft_printf("As this extends standard printf functionality, there is no equivalent test for this. This is here to demonstrate that it won't crash.\n\n");
+
+
+ft_printf("\n\n####\nTESTING MEMORY PRINTING\n#####\n\n");
+int l[] = {3, 255, 256, 257};
+ret1 = ft_printf("%16.16m\n", &l);
+ret2 = printf("03 00 00 00 ff 00 00 00 00 01 00 00 01 01 00 00\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("%16.8m\n", &l);
+ret2 = printf("003 000 000 000 377 000 000 000 000 001 000 000 001 001 000 000\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+ret1 = ft_printf("%8.2m\n", &l);
+ret2 = printf("00000011 00000000 00000000 00000000 11111111 00000000 00000000 00000000\n");
+ret1 != ret2 ? printf("\n\n\n\nERROR\nft: %d\nrl: %d\n\n", ret1, ret2) : 0; printf("\n\n");
+
+
+ret1 = ft_printf("%.2m\n", &l);
+ret1 = ft_printf("%8m\n", &l);
+ret1 = ft_printf("%8.3m\n", &l);
+ret1 = ft_printf("%3.8m\n", &l);
+
+int file = open("test", O_WRONLY);
+ft_printf_fd(file, "Testing ft_printf_fd...\n%f\n%d\n#%-5x#", 123.456, 123456, 253);
+close(file);
+
+*/
+
 return 0;
 }
